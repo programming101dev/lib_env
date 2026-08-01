42737af87b5397f7dc49c54de84d3649c2e5ba8d
programming101dev <113087924+programming101dev@users.noreply.github.com>
2026-08-01 08:06:45 -0700

    Add void wrapper fault injection helper

--- include/p101_env/wrapper.h
+++ include/p101_env/wrapper.h
@@ -52,6 +52,20 @@
             return p101_wrapper_fault_code_;                            \
         }                                                               \
     } while(0)
+
+#define P101_WRAPPER_FAULT_RETURN_VOID(env, err)                         \
+    do                                                                  \
+    {                                                                   \
+        int p101_wrapper_fault_code_;                                   \
+                                                                        \
+        p101_wrapper_fault_code_ = p101_env_check_fault((env), __func__ + 5); \
+        if(p101_wrapper_fault_code_ != 0)                               \
+        {                                                               \
+            P101_ERROR_RAISE_ERRNO((err), p101_wrapper_fault_code_);    \
+            P101_TRACE_EXIT(env);                                       \
+            return;                                                     \
+        }                                                               \
+    } while(0)
 // clang-format on
 
 static inline size_t p101_wrapper_short_count(size_t requested, size_t amount)
